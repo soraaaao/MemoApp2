be1945ec286ad99491569cdefdb7642da1309b81
soraaaaao <soraoooo0714@gmail.com>
2022-09-10 19:02:41 +0900

    Extract components

--- App.jsx
+++ App.jsx
@@ -1,49 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import AppBar from './src/components/AppBar';
+import MemoList from './src/components/MemoList';
+import CirecleButton from './src/components/SircleButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.appbar}>
-        <View style={styles.appbarInner}>
-          <Text style={styles.appbarTitle}>Memo App</Text>
-          <Text style={styles.appbarRight}>ログアウト</Text>
-        </View>
-      </View>
-
-      <View>
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2022年12月24日 10:00</Text>
-          </View>
-          <View>
-            <Text>x</Text>
-          </View>
-        </View>
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2022年12月24日 10:00</Text>
-          </View>
-          <View>
-            <Text>x</Text>
-          </View>
-        </View>
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2022年12月24日 10:00</Text>
-          </View>
-          <View>
-            <Text>x</Text>
-          </View>
-        </View>
-      </View>
-
-      <View style={styles.circleBottom}>
-        <Text style={styles.circleBottomLabel}>+</Text>
-      </View>
+      <AppBar />
+      <MemoList />
+      <CirecleButton>+</CirecleButton>
     </View>
   );
 }
@@ -52,28 +18,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F0F4F8',
-  },
-  appbar: {
-    width: '100%',
-    height: 104,
-    backgroundColor: '#467FD3',
-    justifyContent: 'flex-end',
-  },
-  appbarInner: {
-    alignItems: 'center',
-  },
-  appbarRight: {
-    position: 'absolute',
-    right: 19,
-    bottom: 16,
-    color: 'rgba(255, 255, 255, 0.8)',
-  },
-  appbarTitle: {
-    marginBottom: 8,
-    fontSize: 22,
-    lineHeight: 32,
-    color: '#ffffff',
-    fontWeight: 'bold',
   },
   memoListItem: {
     backgroundColor: '#ffffff',
